82db5f693ba354b9ac5292ceaff5188e94d7d852
PreetSharma-12 <mr.bhardwaz14@gmail.com>
2024-10-16 16:02:53 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,25 @@
+from playsound import playsound
+import time
+
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
+
+def alarm(seconds):
+    time_elapsed = 0
+
+    print(CLEAR)
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+        time_left = seconds - time_elapsed
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
+
+        print(f"{CLEAR_AND_RETURN}Alarm will sound in: {minutes_left:02d}:{seconds_left:02d}")
+
+    playsound("RAUF~FAIK. OP.mp3")
+
+mins = int(input("How many minutes to wait: "))
+secs = int(input("How many seconds to wait: "))
+total_secs = mins * 60 + secs
+alarm(total_secs)
